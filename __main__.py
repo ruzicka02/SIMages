aa06b8ddc17ca0841277c1bea9afc911070955a8
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2024-03-26 11:36:03 +0100

    Selection GUI

--- __main__.py
+++ __main__.py
@@ -8,6 +8,16 @@ import gui
 from nicegui import ui
 from pathlib import Path
 import sys
+import os
+
+os.chdir(Path(__file__).parent)
+print("CWD", os.getcwd())
+
+subdirs = []
+for path in Path("data").iterdir():
+    if path.is_dir():
+        subdirs.append(path)
+gui.draw_selection(subdirs)
 
 img_path = sys.argv[1] if len(sys.argv) > 1 else "data/Italy/IMG20230803112016.jpg"
 img_path = Path(img_path)
@@ -20,6 +30,5 @@ results = nearest.knn_query(img_path, 10, data_path)
 for name, similarity in results:
     print(f"{similarity:7.3f} % - {name} {'(identity)' if name == img_path else ''}")
 
-gui.draw_gui(img_path, results)
-
-ui.run()
+# gui.draw_similar(img_path, results)
+ui.run(title="VWM semestralka", favicon="📷", reload=False)
--- gui.py
+++ gui.py
@@ -1,11 +1,61 @@
 from nicegui import ui
 from pathlib import Path
-from copy import deepcopy
 
-source = "IMG20230803112016.jpg"
-close = ["IMG20230803112019.jpg", "IMG20230802131959.jpg", "IMG20230803112010.jpg", "IMG20230724132147.jpg", "IMG20230803112008.jpg"]
+from random import shuffle
 
-def draw_gui(source_img: Path, close_img: list[tuple[Path, float]]):
+draw_imgs = []
+all_imgs = []
+imgs_visible = 0
+
+@ui.page("/select")
+def draw_selection(available_dirs: list[Path]):
+     @ui.refreshable
+     def image_preview():
+          print("Image preview:", draw_imgs)
+          print(f"{imgs_visible} visible")
+          with ui.row().style('display: flex; flex-wrap: wrap; justify-content: left; padding: 0 4px; margin: auto'):
+               for img in draw_imgs:
+                    with ui.column():
+                         ui.label(img.name)
+                         with ui.button(on_click=lambda local_img=img: print(local_img)).style('padding: 0px'):
+                              print("Draw:", img)
+                              ui.image(img).style('width: 18vw; vertical-align: middle')
+
+     def select_dir(toggle: ui.toggle):
+          global all_imgs
+          global draw_imgs
+          global imgs_visible
+          print("Toggle:", toggle.value)
+          with open(toggle.value / "img_order.txt", "r") as f:
+               new_imgs = [toggle.value / Path(x) for x in f.read().split()]
+               shuffle(new_imgs)
+               all_imgs = new_imgs
+
+          imgs_visible = 10
+          draw_imgs = all_imgs[:imgs_visible]
+          print(draw_imgs)
+          image_preview.refresh()
+
+     def more_photos():
+          global imgs_visible
+          prev = imgs_visible
+          imgs_visible += 10
+          draw_imgs.extend(all_imgs[prev:imgs_visible])
+          image_preview.refresh()
+
+
+     ui.label('SIMages - selection').style('font-size: 4em; font-weight: 300')  # color: #6E93D6;
+
+     # select_dir(dir_toggle, all_imgs, draw_imgs)
+     dir_toggle = ui.toggle({x: str(x) for x in available_dirs}, on_change=lambda: select_dir(dir_toggle))
+
+     image_preview()
+
+     ui.button("More photos", on_click=more_photos)
+
+
+@ui.page("/similar")
+def draw_similar(source_img: Path, close_img: list[tuple[Path, float]]):
      dialog_image_ui = None
 
      def open_dialog(url: str):
@@ -17,7 +67,7 @@ def draw_gui(source_img: Path, close_img: list[tuple[Path, float]]):
      with ui.dialog() as dialog:
           dialog_image_ui = ui.image().style("width: 100%; height: auto")
 
-     ui.label('Image similarity prototype').style('font-size: 4em; font-weight: 300')  # color: #6E93D6;
+     ui.label('SIMages - preview').style('font-size: 4em; font-weight: 300')  # color: #6E93D6;
      # ui.image('data/IMG20230803112016.jpg').classes('w-256')
 
      # source image
@@ -31,6 +81,6 @@ def draw_gui(source_img: Path, close_img: list[tuple[Path, float]]):
                # ui.button("Image 1") ui.button("Image 2", on_click=lambda: open_dialog(image_url_2))
                with ui.column():
                     ui.label(f"{similarity:7.3f} % similarity")
-                    with ui.button(on_click=lambda local_img=img: open_dialog(local_img)).style('margin: 0px'):
+                    with ui.button(on_click=lambda local_img=img: open_dialog(local_img)).style('padding: 0px'):
                          print("Draw:", img)
                          ui.image(img).style('width: 15vw; vertical-align: middle')
